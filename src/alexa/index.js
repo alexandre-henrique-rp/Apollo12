af519d96e08dadb00030540b2ac8f3e0ea71879d
Alexandre Henrique da Rocha Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2022-10-05 18:59:35 -0300

    update

--- src/alexa/index.js
+++ src/alexa/index.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Alexa() {
   return (
-    <iframe src="https://http://alexa.redebrasilrp.com.br/" width={768} height={1366} ></iframe>
+    <iframe src="https://alexa.redebrasilrp.com.br/" width={768} height={1366} ></iframe>
   );
 }
