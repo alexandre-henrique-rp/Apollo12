124e0cccd2e81a07f90b50ac0184ef1df0acb17e
Alexandre Henrique da Rocha Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2022-10-07 00:10:14 -0300

    up

--- src/alexa/index.js
+++ src/alexa/index.js
@@ -2,6 +2,10 @@ import React from "react";
 
 export default function Alexa() {
   return (
-    <iframe src="https://alexa.redebrasilrp.com.br/" width={768} height={1366} ></iframe>
+    <iframe
+      src="https://alexa.redebrasilrp.com.br/"
+      width={768}
+      height={1366}
+    />
   );
 }
--- src/roleta/index.js
+++ src/roleta/index.js
@@ -2,6 +2,15 @@ import React from "react";
 
 export default function Roleta() {
   return (
-    <iframe src="https://redebrasilrp.com.br/sorteio" width={768} height={1366} ></iframe>
+    <iframe
+      src="https://redebrasilrp.com.br/sorteio"
+      width={768}
+      height={1366}
+    />
+    // <iframe
+    //   src="https://alexa.redebrasilrp.com.br/"
+    //   width={768}
+    //   height={1366}
+    // />
   );
 }
